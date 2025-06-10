72e8009ceb4be142f1a2f8894c99c47514fe8212
Zahir-R <zahiriv4@gmail.com>
2025-06-10 12:57:10 -0400

    Create core.js

---
+++ assets/js/core.js
@@ -0,0 +1,34 @@
+import { cargarDatos } from './preguntas.js';
+import { mostrarTeoria } from './teoria.js';
+import { iniciarPractica } from './practica.js';
+import { iniciarSimulacro } from './simulacro.js';
+
+export async function inicializarApp() {
+    const modeSelector = document.getElementById('mode');
+    const topicSelector = document.getElementById('topic');
+    const startBtn = document.getElementById('startBtn');
+
+    let modo = "estudio";
+    let tema = "funciones";
+
+    modeSelector.addEventListener('change', () => {
+        modo = modeSelector.value;
+        topicSelector.disabled = (modo === 'simulacro');
+    });
+
+    startBtn.disabled = true;
+try {
+        await cargarDatos();
+        startBtn.disabled = false;
+    } catch {
+        alert("No se pudieron cargar las preguntas.");
+        startBtn.disabled = false;
+    }
+
+    startBtn.addEventListener('click', () => {
+        tema = topicSelector.value;
+        if (modo === 'estudio') mostrarTeoria(tema);
+        else if (modo === 'practica') iniciarPractica(tema);
+        else if (modo === 'simulacro') iniciarSimulacro();
+    });
+}
