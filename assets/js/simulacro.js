adb09c035463150c967fb97668ae37fc3fe7887c
Zahir-R <zahiriv4@gmail.com>
2025-06-18 13:49:45 -0400

    Update simulacro.js

--- assets/js/simulacro.js
+++ assets/js/simulacro.js
@@ -5,8 +5,9 @@ import { mostrarPreguntas } from './ui.js';
 export function iniciarSimulacro() {
     let todas = [];
     Object.keys(preguntas).forEach(t => {
-        todas = todas.concat(preguntas[t]);
+        const maxQuestions = mezclarArray(preguntas[t]).slice(0, 4); // So that the exam will always cover at least 3 topics
+        todas = todas.concat(maxQuestions);
     });
-    const seleccionadas = mezclarArray(todas).slice(0, 10); // 10 preguntas aleatorias
+    const seleccionadas = mezclarArray(todas).slice(0, 10);
     mostrarPreguntas(seleccionadas, true);
 }
